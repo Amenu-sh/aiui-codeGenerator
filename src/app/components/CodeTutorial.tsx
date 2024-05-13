8c0b20e983de1295ec5ec62ad14cf4e359354711
Amenu-sh <amanushifara@gmail.com>
2024-05-13 14:38:00 -0500

    first commit

---
+++ src/app/components/CodeTutorial.tsx
@@ -0,0 +1,100 @@
+"use client";
+import {
+  useCopilotAction,
+  useMakeCopilotReadable,
+} from "@copilotkit/react-core";
+import Markdown from "react-markdown";
+import { useState } from "react";
+import AceEditor from "react-ace";
+import React from "react";
+
+export default function CodeTutorial() {
+  const [code, setCode] = useState<string[]>([
+    `<h1 class="text-red-500">Hello World</h1>`,
+  ]);
+  const [codeToDisplay, setCodeToDisplay] = useState<string>(code[0] || "");
+    const [codeTutorial, setCodeTutorial] = useState(``);
+    useMakeCopilotReadable(codeToDisplay);
+    useCopilotAction(
+      {
+        name: "generateCodeAndImplementationTutorial",
+        description:
+          "Create Code Snippet with React.js(Next.js), tailwindcss and an implementation tutorial of the code generated.",
+        parameters: [
+          {
+            name: "code",
+            type: "string",
+            description: "Code to be generated",
+            required: true,
+          },
+          {
+            name: "tutorial",
+            type: "string",
+            description:
+              "Markdown of step by step guide tutorial on how to use the generated code accompanied with the code. Include introduction, prerequisites and what happens at every step accompanied with code generated earlier. Don't forget to add how to render the code on browser.",
+            required: true,
+          },
+        ],
+        handler: async ({ code, tutorial }) => {
+          setCode((prev) => [...prev, code]);
+          setCodeToDisplay(code);
+          setCodeTutorial(tutorial);
+        },
+      },
+      [codeToDisplay, codeTutorial]
+    );
+
+  function onChange(newCode: any) {
+    setCodeToDisplay(newCode);
+  }
+
+  return (
+    <>
+      <main className=" min-h-screen px-4">
+        <div className="w-full h-full min-h-[70vh] flex justify-between gap-x-1 ">
+          <div className="w-2/3 min-h-[60vh] rounded-lg bg-white shadow-lg p-2 border mt-8 overflow-auto">
+            <div
+              className="w-full min-h-[60vh] rounded-lg"
+              dangerouslySetInnerHTML={{ __html: codeToDisplay }}
+            />
+          </div>
+          <AceEditor
+            placeholder="Placeholder Text"
+            mode="html"
+            theme="monokai"
+            name="blah2"
+            className="w-[50%] min-h-[60vh] p-2 mt-8 rounded-lg"
+            onChange={onChange}
+            fontSize={14}
+            lineHeight={19}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            value={codeToDisplay}
+            setOptions={{
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+              enableSnippets: false,
+              showLineNumbers: true,
+              tabSize: 2,
+            }}
+          />
+        </div>
+        <div className="w-10/12 mx-auto">
+          <div className="mt-8">
+            <h1 className="text-white text-center text-xl font-semibold p-6">
+              Code Tutorial
+            </h1>
+            {codeTutorial ? (
+              <Markdown className="text-white">{codeTutorial}</Markdown>
+            ) : (
+              <div className="text-white">
+                The Code Tutorial Will Appear Here
+              </div>
+            )}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/Header";
+import CodeTutorial from "./components/CodeTutorial";
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotSidebar } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <CopilotKit url="/api/copilotkit">
+        <CopilotSidebar
+          instructions="Help the user generate code. Ask the user if to generate its tutorial."
+          defaultOpen={true}
+          labels={{
+            title: "Code & Tutorial Generator",
+            initial: "Hi! 👋 I can help you generate code and its tutorial.",
+          }}
+        >
+          <CodeTutorial />
+        </CopilotSidebar>
+      </CopilotKit>
+    </>
+  );
+}
